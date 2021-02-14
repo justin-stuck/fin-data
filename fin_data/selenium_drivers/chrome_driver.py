7d55b879ee8e523486353cd96ea8c42a92e4a594
Justin Stuck <justin.stuck@gmail.com>
2021-02-14 13:37:14 -0600

    lint

--- fin_data/selenium_drivers/chrome_driver.py
+++ fin_data/selenium_drivers/chrome_driver.py
@@ -1,8 +1,9 @@
+from typing import List
+
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
-from typing import List
 
 
 class ChromeDriver:
